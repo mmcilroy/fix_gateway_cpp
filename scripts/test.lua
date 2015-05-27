6817e1ed10e36b351ad7d94ee7a068d5b20f485f
sm87110 <sm87110@dev01.eur.nsroot.net>
2015-05-27 17:39:33 +0100

    asio impl

--- scripts/test.lua
+++ scripts/test.lua
@@ -1,8 +1,16 @@
 
 engine = fix.engine()
 
-function handler( session, message )
-    engine:send( session, message )
+function handler( session, request )
+    print( 'hello' )
+    --[[
+    if request[35] == 'D' then
+        response[35] = '8'
+        response[39] = '0'
+        response[55] = request[55]
+        engine:send( session, response )
+    end
+    ]]
 end
 
 engine:acceptor( 'tcp@localhost:14002', handler )
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include "fixtk/engine.hpp"
+
 extern "C"
 {
 #include <lua.h>
@@ -7,50 +9,17 @@ extern "C"
 
 #include <iostream>
 
-typedef int fix_session_id;
-
-class fix_engine
-{
-public:
-    fix_engine()
-    {
-        std::cout << this << " new engine" << std::endl;
-    }
-
-    ~fix_engine()
-    {
-        std::cout << this << " del engine" << std::endl;
-    }
-
-    template< typename H >
-    void acceptor( const std::string& conn, H handler )
-    {
-        std::cout << this << " acceptor" << std::endl;
-        handler( 1, "hi" );
-    }
-
-    void send( fix_session_id id, const std::string& message )
-    {
-        std::cout << this << " send " << id << ", " << message << std::endl;
-    }
-
-    void start()
-    {
-        std::cout << this << " start" << std::endl;
-    }
-};
-
 const char* engine_id = "luaL_engine";
 
-fix_engine* l_check_fix_engine( lua_State* l, int n )
+engine* l_check_engine( lua_State* l, int n )
 {
-    return *(fix_engine**)luaL_checkudata( l, n, engine_id );
+    return *(engine**)luaL_checkudata( l, n, engine_id );
 }
 
 int l_new_engine( lua_State* l )
 {
-    fix_engine** udata = (fix_engine**)lua_newuserdata( l, sizeof( fix_engine* ) );
-    *udata = new fix_engine();
+    engine** udata = (engine**)lua_newuserdata( l, sizeof( engine* ) );
+    *udata = new engine();
     luaL_getmetatable( l, engine_id );
     lua_setmetatable( l, -2 );
     return 1;
@@ -58,17 +27,17 @@ int l_new_engine( lua_State* l )
 
 int l_del_engine( lua_State* l )
 {
-    delete l_check_fix_engine( l, -1 );
+    delete l_check_engine( l, -1 );
     return 0;
 }
 
 int l_engine_acceptor( lua_State* l )
 {
-    fix_engine* engine = l_check_fix_engine( l, 1 );
+    engine* engine = l_check_engine( l, 1 );
     const char* conn = luaL_checkstring( l, 2 );
     int handler = luaL_ref( l, LUA_REGISTRYINDEX );
 
-    engine->acceptor( "", [&]( fix_session_id id, const std::string& message ) {
+    engine->acceptor( "", [&]( session_id id, const std::string& message ) {
         lua_rawgeti( l, LUA_REGISTRYINDEX, handler );
         lua_pushnumber( l, id );
         lua_pushstring( l, message.c_str() );
@@ -80,7 +49,7 @@ int l_engine_acceptor( lua_State* l )
 
 int l_engine_send( lua_State* l )
 {
-    fix_engine* engine = l_check_fix_engine( l, 1 );
+    engine* engine = l_check_engine( l, 1 );
     int sess = luaL_checknumber( l, 2 );
     const char* message = luaL_checkstring( l, 3 );
     engine->send( sess, message );
@@ -89,7 +58,7 @@ int l_engine_send( lua_State* l )
 
 int l_engine_start( lua_State* l )
 {
-    l_check_fix_engine( l, 1 )->start();
+    l_check_engine( l, 1 )->start();
     return 0;
 }
 
@@ -97,10 +66,10 @@ void l_register( lua_State* l )
 {
     luaL_Reg engine_reg[] = {
         { "engine", l_new_engine },
-        { "__gc", l_del_engine },
         { "acceptor", l_engine_acceptor },
         { "send", l_engine_send },
         { "start", l_engine_start },
+        { "__gc", l_del_engine },
         { NULL, NULL }
     };
 
